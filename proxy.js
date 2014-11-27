3666b23ac45bc491a1aca87d4645bdcf44009153
Mr1024 <publiczhongemail@163.com>
2014-11-27 17:25:18 +0800

    update

--- proxy.js
+++ proxy.js
@@ -2,18 +2,31 @@
 
 var socks = require('socksv5'),
     Connection = require('ssh2'),
-    fs = require('fs');;
+    fs = require('fs'),
+    ping = require('tcp-ping');
+var serverArry = [];
 fs.readFile('./lib/config.json', {
     "encoding": "utf8"
 }, function(err, data) {
     if (!err) {
         var data = JSON.parse(data);
-        console.log(data);
+        for (var item in data) {
+            ping.ping({
+                address: item,
+                port: data[item].port
+            }, function(err, data) {
+                if(!err){
+                  console.log(data);
+                  serverArry.push(data);
+                }
+            });
+            console.log(serverArry);
+        }
     }
 });
 var ssh_config = {
-    host: '63.141.248.218',
-    port: 1308,
+    host: '23.95.57.193',
+    port: 1309,
     username: 'boa',
     password: 'eyo3'
 };
@@ -39,8 +52,10 @@ socks.createServer(function(info, accept, deny) {
                     conn.end();
             });
     }).on('error', function(err) {
+        console.log(err);
         deny();
-    }).connect(ssh_config);
+    });
+    conn.connect(ssh_config);
 }).listen(7070, '127.0.0.1', function() {
     console.log('SOCKSv5 proxy server started on port 7070');
 }).useAuth(socks.auth.None());
