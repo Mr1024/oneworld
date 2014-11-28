6b00d315403b16ca87840bc1fca1373ebc4ce96a
Mr1024 <publiczhongemail@163.com>
2014-11-28 19:18:52 +0800

    update

--- proxy.js
+++ proxy.js
@@ -3,59 +3,62 @@
 var socks = require('socksv5'),
     Connection = require('ssh2'),
     fs = require('fs'),
-    ping = require('tcp-ping');
-var serverArry = [];
+    ping = require('ping-net'),
+    ssh_config,
+    sockerserver;
+
 fs.readFile('./lib/config.json', {
     "encoding": "utf8"
 }, function(err, data) {
     if (!err) {
         var data = JSON.parse(data);
+        var options = [];
         for (var item in data) {
-            ping.ping({
+            options.push({
                 address: item,
                 port: data[item].port
-            }, function(err, data) {
-                if(!err){
-                  console.log(data);
-                  serverArry.push(data);
-                }
             });
-            console.log(serverArry);
         }
+        ping.ping(options, function(result) {
+            var resultdata = result[0];
+            if (!isNaN(resultdata.avg)) {
+                ssh_config = data[resultdata.address];
+                server();
+            }
+
+        }, true);
     }
 });
-var ssh_config = {
-    host: '23.95.57.193',
-    port: 1309,
-    username: 'boa',
-    password: 'eyo3'
-};
 
-socks.createServer(function(info, accept, deny) {
-    var conn = new Connection();
-    console.log(info);
-    conn.on('ready', function() {
-        conn.forwardOut(info.srcAddr,
-            info.srcPort,
-            info.dstAddr,
-            info.dstPort,
-            function(err, stream) {
-                if (err)
-                    return deny();
-
-                var clientSocket;
-                if (clientSocket = accept(true)) {
-                    stream.pipe(clientSocket).pipe(stream).on('close', function() {
+function server() {
+    sockerserver && sockerserver.close();
+    sockerserver = socks.createServer(function(info, accept, deny) {
+        var conn = new Connection();
+        conn.on('ready', function() {
+            conn.forwardOut(info.srcAddr,
+                info.srcPort,
+                info.dstAddr,
+                info.dstPort,
+                function(err, stream) {
+                    if (err)
+                        return deny();
+                    var clientSocket;
+                    if (clientSocket = accept(true)) {
+                        stream.pipe(clientSocket).pipe(stream).on('close', function() {
+                            conn.end();
+                        });
+                    } else
                         conn.end();
-                    });
-                } else
-                    conn.end();
-            });
-    }).on('error', function(err) {
-        console.log(err);
-        deny();
+                });
+        }).on('error', function(err) {
+            console.log(err);
+            //conn.connect(ssh_config);
+            //deny();
+        });
+        conn.connect(ssh_config);
     });
-    conn.connect(ssh_config);
-}).listen(7070, '127.0.0.1', function() {
-    console.log('SOCKSv5 proxy server started on port 7070');
-}).useAuth(socks.auth.None());
+    sockerserver.listen(7070, '127.0.0.1', function() {
+        console.log('SOCKSv5 proxy server started on port 7070');
+    });
+    sockerserver.useAuth(socks.auth.None());
+}
