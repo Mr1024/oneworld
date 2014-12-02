c194bbdb18f3c70ae125dadb6478d916040ec3f4
Mr1024 <publiczhongemail@163.com>
2014-12-02 13:58:35 +0800

    update

--- proxy.js
+++ proxy.js
@@ -5,8 +5,7 @@ var socks = require('socksv5'),
     fs = require('fs'),
     ping = require('ping-net'),
     webspider = require('./lib/webspider'),
-    ssh_config,
-    sockerserver;
+    ssh_config;
 
 init();
 
@@ -37,37 +36,37 @@ function init() {
 }
 
 function server() {
-    sockerserver && sockerserver.close();
-    sockerserver = socks.createServer(function(info, accept, deny) {
-        var conn = new Connection();
-        conn.on('ready', function() {
+    var conn = new Connection();
+    conn.on('ready', function() {
+        console.log("SSH is ready······");
+        socks.createServer(function(info, accept, deny) {
             conn.forwardOut(info.srcAddr,
                 info.srcPort,
                 info.dstAddr,
                 info.dstPort,
                 function(err, stream) {
-                    if (err)
+                    if (err) {
                         return deny();
+                    }
                     var clientSocket;
                     if (clientSocket = accept(true)) {
                         stream.pipe(clientSocket).pipe(stream).on('close', function() {
-                            conn.end();
+
                         });
-                    } else
-                        conn.end();
+                    } else {
+                        //conn.end();
+                    }
                 });
-        }).on('error', function(err) {
-            console.log(err);
-            webspider.connServer(init);
-            conn.connect(ssh_config);
-            //deny();
-        });
-        conn.connect(ssh_config);
+
+        }).listen(7070, '127.0.0.1', function() {
+            console.log('->SOCKSv5 proxy server started on port 7070······');
+        }).useAuth(socks.auth.None());
+    }).on('error', function(err) {
+        console.log(err);
+        conn.end();
+        webspider.connServer(init);
     });
-    sockerserver.listen(7070, '127.0.0.1', function() {
-        console.log('->SOCKSv5 proxy server started on port 7070······');
-    });
-    sockerserver.useAuth(socks.auth.None());
+    conn.connect(ssh_config);
 }
 setInterval(function() {
     webspider.connServer();
